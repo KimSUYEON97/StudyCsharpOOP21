650d86a15d27b5d3e85f5a909fc799a93ce42b5b
KimSUYEON97 <calicitine97@naver.com>
2021-05-04 14:28:50 +0900

    new project생성

---
+++ OopSolution/InterfaceTestApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InterfaceTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
